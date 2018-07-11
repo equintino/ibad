7b6e398dea7f36b74cfe6cb73a25647413ad6775
equintino <emquintino@ig.com.br>
2018-07-10 21:35:21 -0300

    comprovante

--- web/teste/OmieAppAuth.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Omie API Key
-// http://developer.omie.com.br/
-
-define("OMIE_APP_KEY",    "0000030000");
-define("OMIE_APP_SECRET", "aaaaa111111bbbbbbb222222cccccccc");
--- web/teste/externo/form.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="#">
-	<label>Nome:</label>
-	<input type="text" name="nome">
-	<br>
-	<input type="submit">
-</form>
